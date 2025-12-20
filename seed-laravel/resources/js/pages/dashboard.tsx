5db48f6d46c2ab093a59e19f393bee4af4a10253
Shihab Mahamud <shihab4t@gmail.com>
2025-12-20 22:14:05 +0600

    wip

--- seed-laravel/resources/js/pages/dashboard.tsx
+++ seed-laravel/resources/js/pages/dashboard.tsx
@@ -1,19 +1,18 @@
-import AppLayout from '@/components/layouts/app-layout';
 import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
-import { dashboard } from '@/routes';
-import { type TBreadcrumb } from '@/types/props';
 import { Head } from '@inertiajs/react';
 
-const breadcrumbs: TBreadcrumb[] = [
-    {
-        title: 'Dashboard',
-        href: dashboard().url,
-    },
-];
+import { AppLayout } from '@/components/layouts/app-layout';
 
-export default function Dashboard() {
+const Dashboard = () => {
     return (
-        <AppLayout breadcrumbs={breadcrumbs}>
+        <AppLayout
+            breadcrumbs={[
+                {
+                    title: 'Dashboard',
+                    href: 'dashboard',
+                },
+            ]}
+        >
             <Head title="Dashboard" />
             <div className="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
                 <div className="grid auto-rows-min gap-4 md:grid-cols-3">
@@ -27,10 +26,12 @@ export default function Dashboard() {
                         <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
                     </div>
                 </div>
-                <div className="relative min-h-[100vh] flex-1 overflow-hidden rounded-xl border border-sidebar-border/70 md:min-h-min dark:border-sidebar-border">
+                <div className="relative min-h-screen flex-1 overflow-hidden rounded-xl border border-sidebar-border/70 md:min-h-min dark:border-sidebar-border">
                     <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
                 </div>
             </div>
         </AppLayout>
     );
-}
+};
+
+export default Dashboard;
--- seed-laravel/resources/js/pages/welcome.tsx
+++ seed-laravel/resources/js/pages/welcome.tsx
@@ -1,8 +1,11 @@
-import { dashboard, login, register } from '@/routes';
-import { type TShared } from '@/types/props';
-import { Head, Link, usePage } from '@inertiajs/react';
+import type { TShared } from '@/types/props';
 
-export default function Welcome({ canRegister = true }: { canRegister?: boolean }) {
+import { dashboard, login, register } from '@/routes';
+import { usePage } from '@inertiajs/react';
+
+import { Head, Link } from '@inertiajs/react';
+
+const Welcome = ({ canRegister = true }: { canRegister?: boolean }) => {
     const { auth } = usePage<TShared>().props;
 
     return (
@@ -15,7 +18,7 @@ export default function Welcome({ canRegister = true }: { canRegister?: boolean 
                 />
             </Head>
             <div className="flex min-h-screen flex-col items-center bg-[#FDFDFC] p-6 text-[#1b1b18] lg:justify-center lg:p-8 dark:bg-[#0a0a0a]">
-                <header className="mb-6 w-full max-w-[335px] text-sm not-has-[nav]:hidden lg:max-w-4xl">
+                <header className="mb-6 w-full max-w-83.75 text-sm not-has-[nav]:hidden lg:max-w-4xl">
                     <nav className="flex items-center justify-end gap-4">
                         {auth.user ? (
                             <Link
@@ -45,8 +48,8 @@ export default function Welcome({ canRegister = true }: { canRegister?: boolean 
                     </nav>
                 </header>
                 <div className="flex w-full items-center justify-center opacity-100 transition-opacity duration-750 lg:grow starting:opacity-0">
-                    <main className="flex w-full max-w-[335px] flex-col-reverse lg:max-w-4xl lg:flex-row">
-                        <div className="flex-1 rounded-br-lg rounded-bl-lg bg-white p-6 pb-12 text-[13px] leading-[20px] shadow-[inset_0px_0px_0px_1px_rgba(26,26,0,0.16)] lg:rounded-tl-lg lg:rounded-br-none lg:p-20 dark:bg-[#161615] dark:text-[#EDEDEC] dark:shadow-[inset_0px_0px_0px_1px_#fffaed2d]">
+                    <main className="flex w-full max-w-83.75 flex-col-reverse lg:max-w-4xl lg:flex-row">
+                        <div className="flex-1 rounded-br-lg rounded-bl-lg bg-white p-6 pb-12 text-[13px] leading-5 shadow-[inset_0px_0px_0px_1px_rgba(26,26,0,0.16)] lg:rounded-tl-lg lg:rounded-br-none lg:p-20 dark:bg-[#161615] dark:text-[#EDEDEC] dark:shadow-[inset_0px_0px_0px_1px_#fffaed2d]">
                             <h1 className="mb-1 font-medium">Let's get started</h1>
                             <p className="mb-2 text-[#706f6c] dark:text-[#A1A09A]">
                                 Laravel has an incredibly rich ecosystem.
@@ -129,7 +132,7 @@ export default function Welcome({ canRegister = true }: { canRegister?: boolean 
                                 </li>
                             </ul>
                         </div>
-                        <div className="relative -mb-px aspect-[335/376] w-full shrink-0 overflow-hidden rounded-t-lg bg-[#fff2f2] lg:mb-0 lg:-ml-px lg:aspect-auto lg:w-[438px] lg:rounded-t-none lg:rounded-r-lg dark:bg-[#1D0002]">
+                        <div className="relative -mb-px aspect-335/376 w-full shrink-0 overflow-hidden rounded-t-lg bg-[#fff2f2] lg:mb-0 lg:-ml-px lg:aspect-auto lg:w-109.5 lg:rounded-t-none lg:rounded-r-lg dark:bg-[#1D0002]">
                             <svg
                                 className="w-full max-w-none translate-y-0 text-[#F53003] opacity-100 transition-all duration-750 dark:text-[#F61500] starting:translate-y-6 starting:opacity-0"
                                 viewBox="0 0 438 104"
@@ -163,7 +166,7 @@ export default function Welcome({ canRegister = true }: { canRegister?: boolean 
                                 />
                             </svg>
                             <svg
-                                className="relative -mt-[4.9rem] -ml-8 w-[448px] max-w-none lg:-mt-[6.6rem] lg:ml-0 dark:hidden"
+                                className="relative -mt-[4.9rem] -ml-8 w-md max-w-none lg:-mt-[6.6rem] lg:ml-0 dark:hidden"
                                 viewBox="0 0 440 376"
                                 fill="none"
                                 xmlns="http://www.w3.org/2000/svg"
@@ -480,7 +483,7 @@ export default function Welcome({ canRegister = true }: { canRegister?: boolean 
                                 </g>
                             </svg>
                             <svg
-                                className="relative -mt-[4.9rem] -ml-8 hidden w-[448px] max-w-none lg:-mt-[6.6rem] lg:ml-0 dark:block"
+                                className="relative -mt-[4.9rem] -ml-8 hidden w-md max-w-none lg:-mt-[6.6rem] lg:ml-0 dark:block"
                                 viewBox="0 0 440 376"
                                 fill="none"
                                 xmlns="http://www.w3.org/2000/svg"
@@ -796,4 +799,6 @@ export default function Welcome({ canRegister = true }: { canRegister?: boolean 
             </div>
         </>
     );
-}
+};
+
+export default Welcome;
