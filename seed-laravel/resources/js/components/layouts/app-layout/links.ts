f16a74ca3a8fb066fbf3389480e50d6a773aabf5
Shihab Mahamud <shihab4t@gmail.com>
2025-12-20 23:39:58 +0600

    links

--- seed-laravel/resources/js/components/layouts/app-layout/links.ts
+++ seed-laravel/resources/js/components/layouts/app-layout/links.ts
@@ -1,29 +1,34 @@
 import type { TLink } from '@/types/utils';
 
-import { BookOpen, Folder, LayoutGrid, StickyNote } from 'lucide-react';
+import { Archive, Folder, Star, StickyNote, Trash2 } from 'lucide-react';
 
 export const mainLinks: TLink[] = [
-    {
-        title: 'Dashboard',
-        route: 'dashboard',
-        icon: LayoutGrid,
-    },
     {
         title: 'Notes',
         route: 'notes.index',
         icon: StickyNote,
+    },
+    {
+        title: 'Favorites',
+        route: 'notes.index',
+        icon: Star,
+    },
+    {
+        title: 'Archived',
+        route: 'notes.index',
+        icon: Archive,
+    },
+    {
+        title: 'Trash',
+        route: 'notes.index',
+        icon: Trash2,
     },
 ];
 
 export const rightLinks: TLink[] = [
     {
         title: 'Repository',
-        href: 'https://github.com/laravel/react-starter-kit',
+        href: 'https://github.com/mt-shihab26/seed/tree/main/seed-laravel',
         icon: Folder,
-    },
-    {
-        title: 'Documentation',
-        href: 'https://laravel.com/docs/starter-kits#react',
-        icon: BookOpen,
     },
 ];
