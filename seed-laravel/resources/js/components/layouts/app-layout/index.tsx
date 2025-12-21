8ccc547c7e12413ece097127b59455c8e2efda84
Shihab Mahamud <shihab4t@gmail.com>
2025-12-21 13:45:04 +0600

    wip

--- seed-laravel/resources/js/components/layouts/app-layout/index.tsx
+++ seed-laravel/resources/js/components/layouts/app-layout/index.tsx
@@ -1,19 +1,10 @@
-import type { TBreadcrumb } from '@/types/utils';
 import type { ReactNode } from 'react';
 
-import { Header } from './header';
 import { Menu } from './menu';
 
-export const AppLayout = ({
-    children,
-    breadcrumbs,
-}: {
-    children: ReactNode;
-    breadcrumbs?: TBreadcrumb[];
-}) => {
+export const AppLayout = ({ children }: { children: ReactNode }) => {
     return (
         <div className="flex min-h-screen w-full flex-col">
-            <Header breadcrumbs={breadcrumbs} />
             <div className="flex flex-col items-center justify-center">
                 <Menu />
             </div>
