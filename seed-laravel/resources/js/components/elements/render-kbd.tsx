7fea7a806c9a48f7cbc9dd5e32ea3ce1d00a7a72
Shihab Mahamud <shihab4t@gmail.com>
2025-12-21 23:26:49 +0600

    wip

--- seed-laravel/resources/js/components/elements/render-kbd.tsx
+++ seed-laravel/resources/js/components/elements/render-kbd.tsx
@@ -1,16 +1,17 @@
 import { formatKey } from '@/lib/shortcut';
 
 import { Kbd, KbdGroup } from '@/components/ui/kbd';
+import { Fragment } from 'react';
 
 export const RenderKDB = ({ keys }: { keys: string[] }) => {
     return (
         <div className="flex flex-col items-center gap-4">
             <KbdGroup>
                 {keys.map((key, index) => (
-                    <>
+                    <Fragment key={key}>
                         {!!index && <span>+</span>}
                         <Kbd key={`${key}-${index}`}>{formatKey(key)}</Kbd>
-                    </>
+                    </Fragment>
                 ))}
             </KbdGroup>
         </div>
