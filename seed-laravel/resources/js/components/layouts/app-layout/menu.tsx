d004b62cf947314a717eed966de05cfcc2d1f479
Shihab Mahamud <shihab4t@gmail.com>
2025-12-21 15:56:56 +0600

    wip

--- seed-laravel/resources/js/components/layouts/app-layout/menu.tsx
+++ seed-laravel/resources/js/components/layouts/app-layout/menu.tsx
@@ -45,7 +45,7 @@ export const Menu = () => {
                 </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent
-                className="min-w-full space-y-3 rounded-4xl p-4 md:min-w-120 md:p-6"
+                className="min-w-full space-y-3 rounded-4xl p-4 md:min-w-120 md:p-6 overflow-hidden"
                 align="center"
                 sideOffset={-44}
             >
@@ -61,7 +61,7 @@ export const Menu = () => {
                 </div>
                 <DropdownMenuSeparator />
 
-                <div className="max-h-screen w-full overflow-y-auto overflow-x-hidden">
+                <div className="max-h-screen w-full overflow-x-hidden overflow-y-auto [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-thumb]:bg-gray-300 dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500 [&::-webkit-scrollbar-track]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 dark:[&::-webkit-scrollbar-track]:bg-neutral-700">
                     <DropdownMenuGroup>
                         {pagesLinks.map((link) => (
                             <DropdownMenuItem
