cb78b809cfbd9d5003e7b34c80cf00e6634197c3
Shihab Mahamud <shihab4t@gmail.com>
2025-12-24 03:39:49 +0600

    fix application layout

--- seed-laravel/resources/js/components/layouts/app-layout/index.tsx
+++ seed-laravel/resources/js/components/layouts/app-layout/index.tsx
@@ -6,11 +6,11 @@ import { Menu } from './menu';
 export const AppLayout = ({ children }: { children: ReactNode }) => {
     return (
         <RootLayout>
-            <div className="flex h-screen w-full flex-col overflow-hidden">
+            <div className="flex h-screen w-full flex-col overflow-hidden pb-2">
                 <div className="flex flex-col items-center justify-center py-1">
                     <Menu />
                 </div>
-                <main className="mx-auto flex h-full w-full max-w-7xl flex-1 flex-col gap-4">
+                <main className="mx-auto flex w-full max-w-7xl flex-1 flex-col gap-4 overflow-hidden">
                     {children}
                 </main>
             </div>
