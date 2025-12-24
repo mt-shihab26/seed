424f12a958f0a54b811a856ca78073a44d548676
Shihab Mahamud <shihab4t@gmail.com>
2025-12-24 16:11:13 +0600

    notes

--- seed-laravel/resources/js/pages/notes/index.tsx
+++ seed-laravel/resources/js/pages/notes/index.tsx
@@ -1,14 +1,29 @@
 import type { TNote } from '@/types/models';
 
 import { AppLayout } from '@/components/layouts/app-layout';
-import { Notes } from '@/components/screens/notes';
+import { Header } from '@/components/screens/notes/header';
+import { NoNotesMessage } from '@/components/screens/notes/no-notes-message';
+import { NoteCard } from '@/components/screens/notes/note-card';
 import { Head } from '@inertiajs/react';
 
 const Index = ({ notes, title }: { notes: TNote[]; title: string }) => {
     return (
         <AppLayout>
             <Head title={title} />
-            <Notes title={title} notes={notes} />
+            <main className="mx-auto flex w-full max-w-7xl flex-1 flex-col space-y-6 overflow-hidden">
+                <Header count={notes?.length} title={title} />
+                <div className="flex-1 space-y-6 overflow-y-auto pr-2.5 [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-thumb]:bg-border [&::-webkit-scrollbar-track]:rounded-full [&::-webkit-scrollbar-track]:bg-muted">
+                    {notes.length === 0 ? (
+                        <NoNotesMessage />
+                    ) : (
+                        <div className="grid gap-4 sm:grid-cols-2 xl:grid-cols-3">
+                            {notes.map((note) => (
+                                <NoteCard key={note.id} note={note} />
+                            ))}
+                        </div>
+                    )}
+                </div>
+            </main>
         </AppLayout>
     );
 };
